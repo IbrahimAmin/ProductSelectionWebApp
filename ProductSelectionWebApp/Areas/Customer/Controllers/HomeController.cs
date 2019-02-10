ca0df6661fa4fcae64338ba3390e7951b8a12ac6
Ibrahim Amin <ibrahim.s.amin@gmail.com>
2019-02-10 20:58:25 +1100

    Main route fixed

--- ProductSelectionWebApp/Areas/Customer/Controllers/HomeController.cs
+++ ProductSelectionWebApp/Areas/Customer/Controllers/HomeController.cs
@@ -46,10 +46,7 @@ namespace ProductSelectionWebApp.Controllers
             return View(ProductCategoryVM);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
